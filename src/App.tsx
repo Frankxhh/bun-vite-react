9a84020ef14e37f744c929597db5831e4678a7bf
zhangxinhui <xinhui.zhang@wuitu.com>
2025-05-07 17:18:56 +0800

    feat: 更改项目结构:hono-vite-jsx-尝试部署vercel;

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello BUN</h1>
-    </div>
-  )
-}
-export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Change the import to use your runtime specific build
+import build from "@hono/vite-build/node";
+
+export default defineConfig(({ mode }) => {
+  if (mode === "client")
+    return {
+      esbuild: {
+        jsxImportSource: "hono/jsx/dom", // Optimized for hono/jsx/dom
+      },
+      build: {
+        rollupOptions: {
+          input: "./src/client.tsx",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    };
+
+  return {
+    plugins: [
+      build({
+        entry: "src/index.tsx",
+      }),
+      devServer({
+        entry: "src/index.tsx",
+      }),
+    ],
+  };
+});
